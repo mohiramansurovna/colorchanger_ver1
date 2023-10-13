e7cbe0d68fb6d5e36dd9b9861a655918fc1af779
mohiramansurovna <147873256+mohiramansurovna@users.noreply.github.com>
2023-10-13 23:52:28 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+function color1(){
+    let elem=document.querySelector('body');
+    elem.style.backgroundColor='#0f4ca7';
+};
+function color2(){
+    let elem=document.querySelector('body');
+    elem.style.backgroundColor='#0fa779';
+};
+function color3(){
+    let elem=document.querySelector('body');
+    elem.style.backgroundColor='#a7650f';
+};
+function color4(){
+    let elem=document.querySelector('body');
+    elem.style.backgroundColor='#5e0fa7';
+};
+function color5(){
+    let elem=document.querySelector('body');
+    elem.style.backgroundColor='#1b6437';
+};
+function color6(){
+    let elem=document.querySelector('body');
+    elem.style.backgroundColor='#120774';
+};
+function color7(){
+    let elem=document.querySelector('body');
+    elem.style.backgroundColor='#a32c0e';
+};
+
+function color8(){
+    let elem=document.querySelector('body');
+    elem.style.backgroundColor='#9da70f';
+};
+
+function color9(){
+    let elem=document.querySelector('body');
+    elem.style.backgroundColor='#4c8813';
+};
